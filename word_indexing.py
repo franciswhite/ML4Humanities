34c76ee153f3f5e85cb1ee39518d58d8dee4c2e5
franciswhite <silvan.hungerbuehler@bluewin.ch>
2017-06-20 15:25:53 +0200

    debugged. done

--- word_indexing.py
+++ word_indexing.py
@@ -1,16 +1,17 @@
 #indexing "conceptual scheme" by its word-count position
 import numpy as np
-
+import string
 collective_corpus = []
 word_count = 0  #init word counter
 occurrence_tracker = [] #init empty list to note word count where bigram occurs
 first_part = "nope" #init first element of bigram
 with open("toy_corpus.txt", 'r') as temp0:
     for line in temp0:
-        collective_corpus.append(line.replace('.', " ", 1).replace('?'," ", 1).replace(',', " ", 1).replace(':', " ", 1).replace(';', " ", 1).replace('!', " ", 1).replace("'", " ", 1).split()) #removes points etc
-        print(collective_corpus)
+        #collective_corpus.append(line.replace('.', " ", 1).replace('?'," ", 1).replace(',', " ", 1).replace(':', " ", 1).replace(';', " ", 1).replace('!', " ", 1).replace("'", " ", 1).split()) #removes points etc
+        collective_corpus.append(line.translate(None, string.punctuation))
 #upper-case lower-case should not matter!!!
     for line in collective_corpus:
+        line = line.split(" ")
         for word in line:
             word_count += 1
             print(word, word_count)
@@ -18,8 +19,7 @@ with open("toy_corpus.txt", 'r') as temp0:
                 first_part = "conceptual"
             else:
                 pass
-            if ((word.lower() == "scheme") or (word.lower() == "schemata") or (word.lower() == "schemes")) and (first_part.lower() == "conceptual"): #smth wrong with this statement
-                print("here2")
+            if ((word.lower() == "scheme") or (word.lower() == "schemata") or (word.lower() == "schemes")) and (first_part.lower() == "conceptual"):
                 occurrence_tracker.append(word_count)
                 first_part = word #setting first word of bigram to not-"conceptual" again
             else:
@@ -29,3 +29,4 @@ with open("toy_corpus.txt", 'r') as temp0:
 time_points = np.array(occurrence_tracker)
 print(time_points)
 
+###Weirdly does not replace all commas.
